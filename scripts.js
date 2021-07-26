623bba1c4b7bb023d78c6c1aec8dce1221f63910
oliverw10 <oliver@warrickfamily.com>
2021-07-26 21:31:19 +1000

    made collabsables better

--- scripts.js
+++ scripts.js
@@ -3,25 +3,28 @@ var i;
 
 var selectedProject = undefined;
 
+const getYPos = (el)=>window.scrollY + el.getBoundingClientRect().top;
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    // unset last selected
+    // remve last selected
     if(selectedProject){
         var oldChild = selectedProject.children[1];
         selectedProject.style.width = "100%";
-        setTimeout(()=>{oldChild.style.maxWidth="0vw"}, 1300);
-        setTimeout(()=>{oldChild.style.maxHeight="5vh"; oldChild.style.top="0%"}, 700)
+        setTimeout(()=>{oldChild.style.maxWidth="0vw"}, 600);
+        oldChild.style.maxHeight="5vh";
+        oldChild.style.top="0%";
     }
 
-    // set this
+    // expand this one
     selectedProject = this;
-    this.classList.toggle("active");
-    var child = this.children[1];
-    var parent = this.parentElement;
-    parent.style.width = "20%";
-    this.style.width = "110%";
-    setTimeout(()=>{child.style.maxWidth="50vw"}, 700);
-    setTimeout(()=>{child.style.maxHeight="60vh"; child.style.top="-100%"}, 1300)
-    // 
+    var child = this.children[1]; // the collapsable
+    var parent = this.parentElement; // the projectContainer
+    parent.style.width = "20%"; // makes all cards move to side
+    this.style.width = "110%"; // make this card slightly wider
+    let yPos = getYPos(parent)-getYPos(child); // gets the relative offset that would be the top of the projectContainer
+    child.style.top = 0; // sets the collabsable to start at where its card is
+    setTimeout(()=>{child.style.maxWidth="50vw"}, 700); // in 700ms make collabsable wide
+    setTimeout(()=>{child.style.maxHeight="55vh"; child.style.top=`${yPos}px`}, 1300);
   });
 }
