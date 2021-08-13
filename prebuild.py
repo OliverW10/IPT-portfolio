a7a5198c1ee6e56bf7123d30dec54ff9c8384b5f
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2021-08-14 09:48:02 +1000

    made versions always up to date

--- prebuild.py
+++ prebuild.py
@@ -6,5 +6,7 @@ if os.path.exists("build"): # if build exists
 os.mkdir("build") # and create it again
 
 shutil.copy("index.html", "build/index.html")
+shutil.copy("styles.css", "build/styles.css")
+shutil.copy("favicon.ico", "build/favicon.ico")
 shutil.copytree("images", "build/images")
 
