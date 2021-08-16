31b5047f8ebf0a5efa49ef1dd8fcbc50ba209660
OliverW10 <34265013+OliverW10@users.noreply.github.com>
2021-08-16 13:58:50 +1000

    tried to fix scrolling and removed unsafe inline js

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 [View the website here](https://oliverw10.github.io/IPT-portfolio/)
 
+
+`versionifier.py` fetches previous versions from github releases, caches their pages and creates links on the page to them
+
+
 (Assuming you have node/npm and python installed) run `npm install` to install local dependancies (minify and gh-pages)
-`npm run build` to create the build files (minifies js and creates previous versions)
+
+`npm run build` to create the build files (minifies js and gets previous versions)
+
 or
+
 `npm run deploy` to create the build files and deploy them to github pages
--- scripts.js
+++ scripts.js
@@ -1,110 +1,112 @@
 
-
 let overlay = document.getElementById("overlay")
-window.addEventListener("load", ()=>{console.log("loaded");overlay.style.opacity="0"})
+window.addEventListener("load", ()=>{doThings(); console.log("loaded");overlay.style.opacity="0"})
 
-var coll = document.getElementsByClassName("projectItem");
-var i;
+// wrapes everthing so that the entire document is loaded before js runs
+function doThings(){
+  var coll = document.getElementsByClassName("projectItem");
+  var i;
 
-var selectedProject = undefined;
+  var selectedProject = undefined;
 
-const getYPos = (el)=>window.scrollY + el.getBoundingClientRect().top;
+  const getYPos = (el)=>window.scrollY + el.getBoundingClientRect().top;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    // remve last selected
-    if(selectedProject){
-        var oldChild = selectedProject.children[1];
-        selectedProject.style.width = "100%";
-        setTimeout(()=>{oldChild.style.maxWidth="0vw"}, 600);
-        oldChild.style.maxHeight="5vh";
-        oldChild.style.top="0%";
-    }
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      // remve last selected
+      if(selectedProject){
+          var oldChild = selectedProject.children[1];
+          selectedProject.style.width = "100%";
+          setTimeout(()=>{oldChild.style.maxWidth="0vw"}, 600);
+          oldChild.style.maxHeight="5vh";
+          oldChild.style.top="0%";
+      }
 
-    // expand this one
-    selectedProject = this;
-    var child = this.children[1]; // the collapsable
-    var parent = this.parentElement; // the projectContainer
-    parent.style.width = "20%"; // makes all cards move to side
-    this.style.width = "110%"; // make this card slightly wider
-    let yPos = getYPos(parent)-getYPos(child); // gets the relative offset that would be the top of the projectContainer
-    child.style.top = 0; // sets the collabsable to start at where its card is
-    let maxHeight = document.getElementById("projectContainer").scrollHeight;
-    setTimeout(()=>{child.style.maxWidth="50vw"}, 700); // in 700ms make collabsable wide
-    setTimeout(()=>{child.style.maxHeight=`${maxHeight}px`; child.style.top=`${yPos}px`}, 1300);
-  });
-}
-
-
-let outerDiv = document.getElementById("outerPage");
-let scroller = document.getElementById("scroller");
-
-let mainDiv = document.getElementById("mainDiv");
-
-
-let lastScroll = 0; // scroll value on the last frame
-let smoothness = 20; // how much to smooth the scrolling higher is less smoothing, gets overridden by slider default value
-
-let isPhone;
-function checkSize(){
-  if (window.matchMedia("(orientation: portrait)").matches) {
-    isPhone = true;
-  } else {
-    isPhone = false;
+      // expand this one
+      selectedProject = this;
+      var child = this.children[1]; // the collapsable
+      var parent = this.parentElement; // the projectContainer
+      parent.style.width = "20%"; // makes all cards move to side
+      this.style.width = "110%"; // make this card slightly wider
+      let yPos = getYPos(parent)-getYPos(child); // gets the relative offset that would be the top of the projectContainer
+      child.style.top = 0; // sets the collabsable to start at where its card is
+      let maxHeight = document.getElementById("projectContainer").scrollHeight;
+      setTimeout(()=>{child.style.maxWidth="50vw"}, 700); // in 700ms make collabsable wide
+      setTimeout(()=>{child.style.maxHeight=`${maxHeight}px`; child.style.top=`${yPos}px`}, 1300);
+    });
   }
-  console.log(isPhone)
-}
-checkSize()
-window.onresize = checkSize
 
-let lastTick = performance.now()
-function updateScroll(time){
-  let delta = time=lastTick
-  lastTick = time
 
-  idealScroll = -window.innerHeight/2-window.scrollY
-  if(!isPhone){
-    // interpolates between lastScroll and ideaScroll relative to delta
-    setScroll = lastScroll*(1-smoothness/delta)  +  idealScroll*(smoothness/delta);
-    outerDiv.style.transform = `translate(0%, ${setScroll}px)`;
-    lastScroll = setScroll;
-  }else{
-    outerDiv.style.transform = `translate(0%, ${idealScroll}px)`;
+  let outerDiv = document.getElementById("outerPage");
+  let scroller = document.getElementById("scroller");
+
+  let mainDiv = document.getElementById("mainDiv");
+
+
+  let lastScroll = 0; // scroll value on the last frame
+  let smoothness = 20; // how much to smooth the scrolling higher is less smoothing, gets overridden by slider default value
+
+  let isPhone;
+  function checkSize(){
+    if (window.matchMedia("(orientation: portrait)").matches) {
+      isPhone = true;
+    } else {
+      isPhone = false;
+    }
+    console.log(isPhone)
+  }
+  checkSize()
+  window.onresize = checkSize
+
+  let lastTick = performance.now()
+  function updateScroll(time){
+    let delta = time=lastTick
+    lastTick = time
+
+    idealScroll = -window.innerHeight/2-window.scrollY
+    if(!isPhone){
+      // interpolates between lastScroll and ideaScroll relative to delta
+      setScroll = lastScroll*(1-smoothness/delta)  +  idealScroll*(smoothness/delta);
+      outerDiv.style.transform = `translate(0%, ${setScroll}px)`;
+      lastScroll = setScroll;
+    }else{
+      outerDiv.style.transform = `translate(0%, ${idealScroll}px)`;
+    }
+    requestAnimationFrame(updateScroll)
   }
   requestAnimationFrame(updateScroll)
-}
-requestAnimationFrame(updateScroll)
 
 
-let sliderVal = document.getElementById("sliderVal")
-let slider = document.getElementById("slider")
-slider.oninput = function (){
-  sliderVal.innerHTML = this.value;
-  smoothness = this.value;
-}
-sliderVal.innerHTML = slider.value;
-smoothness = slider.value;
+  let sliderVal = document.getElementById("sliderVal")
+  let slider = document.getElementById("slider")
+  slider.oninput = function (){
+    sliderVal.innerHTML = this.value;
+    smoothness = this.value;
+  }
+  sliderVal.innerHTML = slider.value;
+  smoothness = slider.value;
 
-// https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
-// embedes another html file
-(() => {
-  const includes = document.getElementsByTagName('include');
-  [].forEach.call(includes, i => {
-      let filePath = i.getAttribute('src');
-      fetch(filePath).then(file => {
-          file.text().then(content => {
-              i.insertAdjacentHTML('afterend', content);
-              i.remove();
-              setDocHeight()
-          });
-      });
-  });
-})();
+  // https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
+  // embedes another html file
+  (() => {
+    const includes = document.getElementsByTagName('include');
+    [].forEach.call(includes, i => {
+        let filePath = i.getAttribute('src');
+        fetch(filePath).then(file => {
+            file.text().then(content => {
+                i.insertAdjacentHTML('afterend', content);
+                i.remove();
+                setDocHeight()
+            });
+        });
+    });
+  })();
 
-function setDocHeight(){
-  const overallHeight = outerDiv.clientHeight;
-  // scroller.style.height = overallHeight;
-  scroller.style.height = overallHeight*1.1;
-  // console.log(mainDiv)
-  // console.log(overallHeight);
+  function setDocHeight(){
+    const overallHeight = outerDiv.clientHeight;
+    // scroller.style.height = overallHeight;
+    scroller.style.height = overallHeight*1.1;
+    // console.log(mainDiv)
+    // console.log(overallHeight);
+  }
 }
